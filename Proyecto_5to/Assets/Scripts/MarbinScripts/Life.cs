e9dcab570756a2f5c90e286dc4e17e9ff5f01aa6
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2022-09-12 08:35:43 -0500

    Vida del personaje

---
+++ Proyecto_5to/Assets/Scripts/MarbinScripts/Life.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Life : MonoBehaviour
+{
+    [Header("Amount life")]
+    [SerializeField] private int life = 100;
+
+
+    public void Update()
+    {
+        LifeDestroy();
+    }
+
+    public void LifeDestroy()
+    {
+        // Si la vida en menor a 0 que el protagonista se destruya
+        if (life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Si toca al enemigo que le baje vida
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            life--;
+        }
+    }
+
+}
