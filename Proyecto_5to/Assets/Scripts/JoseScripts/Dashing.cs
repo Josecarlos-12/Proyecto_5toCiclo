5a999d045644ec1a82db89adedd6aad27a22d8ec
Josecarlos-12 <85040568+Josecarlos-12@users.noreply.github.com>
2022-09-22 16:58:51 -0500

    Cinemachine

---
+++ Proyecto_5to/Assets/Scripts/JoseScripts/Dashing.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dashing : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+}
