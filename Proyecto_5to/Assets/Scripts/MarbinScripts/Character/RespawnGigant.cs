91780ce139cf3774b336a92a8e9db960741ee64a
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2022-10-05 23:24:27 -0500

    Teletransportación

--- Proyecto_5to/Assets/Scripts/MarbinScripts/Character/RespawnGigant.cs
+++ Proyecto_5to/Assets/Scripts/MarbinScripts/Character/RespawnGigant.cs
@@ -7,6 +7,8 @@ public class RespawnGigant : MonoBehaviour
     public GameObject module;
     public Transform prota;
     public Transform floor;
+    public Transform[] next;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Module"))
@@ -21,5 +23,33 @@ public class RespawnGigant : MonoBehaviour
         {
             prota.position=new Vector3(module.transform.position.x, module.transform.position.y+3, module.transform.position.z);
         }
+
+        #region Next
+        if (other.gameObject.name == "NextOne")
+        {
+            prota.position=new Vector3(next[0].position.x, next[0].position.y+3, next[0].position.z);
+        }
+        if (other.gameObject.name == "NextTwo")
+        {
+            prota.position = new Vector3(next[1].position.x, next[1].position.y + 3, next[1].position.z);
+        }
+        if (other.gameObject.name == "NextThree")
+        {
+            prota.position = new Vector3(next[2].position.x, next[2].position.y + 3, next[2].position.z);
+        }
+        if (other.gameObject.name == "NextFour")
+        {
+            prota.position = new Vector3(next[3].position.x, next[3].position.y + 3, next[3].position.z);
+        }
+        if (other.gameObject.name == "NextFive")
+        {
+            prota.position = new Vector3(next[4].position.x, next[4].position.y + 3, next[4].position.z);
+        }
+        if (other.gameObject.name == "NextSix")
+        {
+            prota.position = new Vector3(next[5].position.x, next[5].position.y + 3, next[5].position.z);
+        }
+        #endregion
+
     }
 }
