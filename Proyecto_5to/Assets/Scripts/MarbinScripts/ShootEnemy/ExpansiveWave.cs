d4c1e6604b05ab3d4c3cc3330cc8c20e299d1fde
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2022-10-06 11:50:47 -0500

    Movimiento jefe

--- Proyecto_5to/Assets/Scripts/MarbinScripts/ShootEnemy/ExpansiveWave.cs
+++ Proyecto_5to/Assets/Scripts/MarbinScripts/ShootEnemy/ExpansiveWave.cs
@@ -1,20 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class ExpansiveWave : MonoBehaviour
 {
+    [Header("Movimiento")]
+    public Transform[] points;
+    public NavMeshAgent agent;
+    public int destPoint = 0;
+
+
+
+    [Header("Onda Expansiva")]
     public float radius;
     public float forceExplotion;
 
-   
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Y))
+        if (agent.remainingDistance < 0.5f)
+        {
+            GotoNextPoint();
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.Y))
         {
             Explotion();
             Debug.Log("Explosion");
+        }*/
+    }
+
+
+    public void GotoNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            return;
         }
+
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;       
     }
 
     public void Explotion()
