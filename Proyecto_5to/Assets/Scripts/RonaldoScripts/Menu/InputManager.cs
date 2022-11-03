5556cbcd58fe727ff5121f177ee4bf4f28b3847e
itzslowerdtc <itzslowerdtc@hotmail.com>
2022-11-03 01:18:20 -0500

    inputs corregidos

--- Proyecto_5to/Assets/Scripts/RonaldoScripts/Menu/InputManager.cs
+++ Proyecto_5to/Assets/Scripts/RonaldoScripts/Menu/InputManager.cs
@@ -1,21 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
-public class MenuKey : MonoBehaviour
+public class InputManager : MonoBehaviour
 {
-    public TMP_Text ButtonText;
-    public KeyCode NewKey;
+    [SerializeField]
 
     void Start()
     {
         
     }
-
     void Update()
     {
         
+    }
+    void Change()
+    {
+
     }
 }
