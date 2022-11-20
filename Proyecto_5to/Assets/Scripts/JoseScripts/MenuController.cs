2d54514e6f410caf26ab6314a43defd037a7931f
Josecarlos-12 <85040568+Josecarlos-12@users.noreply.github.com>
2022-11-19 21:24:06 -0500

    Menu Sonidos

--- Proyecto_5to/Assets/Scripts/JoseScripts/MenuController.cs
+++ Proyecto_5to/Assets/Scripts/JoseScripts/MenuController.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 100;
+
+    [Header("Confirmation")]
+    [SerializeField] private GameObject comfirmationPrompt=null;
+
     [Header("Levels To Load")]
     public string _newGameLevel;
     private string levelToLoad;
@@ -32,5 +41,35 @@ public class MenuController : MonoBehaviour
     public void ExitButton( )
     {
         Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    { 
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0");
+    }
+
+    public void VolumeApply( )
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if(MenuType == "Audio" )
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeTextValue.text = defaultVolume.ToString("0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox( )
+    {
+        comfirmationPrompt.SetActive(true);
+        yield return new WaitForSeconds(2);
+        comfirmationPrompt.SetActive(false);
     }
 }
