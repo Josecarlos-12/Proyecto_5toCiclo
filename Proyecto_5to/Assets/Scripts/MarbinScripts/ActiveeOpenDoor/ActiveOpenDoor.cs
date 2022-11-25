e049981d8382562ca70a40400894aa5d8d5cf849
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2022-11-25 00:06:24 -0500

    Extras

--- Proyecto_5to/Assets/Scripts/MarbinScripts/ActiveeOpenDoor/ActiveOpenDoor.cs
+++ Proyecto_5to/Assets/Scripts/MarbinScripts/ActiveeOpenDoor/ActiveOpenDoor.cs
@@ -66,33 +66,8 @@ public class ActiveOpenDoor : MonoBehaviour
                 anim3.SetBool("Open", true);
             }
 
-            if (open)
-            {
-                if(count<3)
-                count++;
 
-                if(count==1)
-                BoosCamera();
-                if (count == 2  || count == 3)
-                {
-                    res.prota.position = new Vector3(res.next[5].position.x, res.next[5].position.y + 3, res.next[5].position.z);
-                    res.prota.rotation = res.next[5].rotation;
-                }
-            }
         }
     }
-    public void BoosCamera()
-    {
-        prota.SetActive(false);
-        cam.SetActive(true);
-        doorDown.SetBool("Open", true);
-        StartCoroutine(Camerafalse());
-    }
-
-    public IEnumerator Camerafalse()
-    {
-        yield return new WaitForSeconds(3);
-        prota.SetActive(true);
-        cam.SetActive(false);
-    }
+   
 }
