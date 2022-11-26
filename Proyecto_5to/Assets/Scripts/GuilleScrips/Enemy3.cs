480c05f4b47699029c5f8383bca67f2a950fb13e
Ronaldo Beltran <49047012+iTzSlower@users.noreply.github.com>
2022-11-26 15:59:53 -0500

    change

--- Proyecto_5to/Assets/Scripts/GuilleScrips/Enemy3.cs
+++ Proyecto_5to/Assets/Scripts/GuilleScrips/Enemy3.cs
@@ -135,5 +135,4 @@ public class Enemy3 : MonoBehaviour
 
     }
 
-
 }
--- Proyecto_5to/Assets/Scripts/MarbinScripts/NavMesh/NavigationBaker.cs
+++ Proyecto_5to/Assets/Scripts/MarbinScripts/NavMesh/NavigationBaker.cs
@@ -5,36 +5,36 @@ using UnityEngine.AI;
 
 public class NavigationBaker : MonoBehaviour
 {
-    public NavMeshSurface[] surfaces;
-    public Transform[] objectsToRotate;
-    public int count;
-
-    void Update()
-    {        
-
-        for (int i = 0; i < surfaces.Length; i++)
-        {
-            //surfaces[i].BuildNavMesh();
-        }
-    }
-
-    public void BuildNavMesh()
-    {
-        if(count<3)
-        count++;
-
-        if (count == 1)
-        {
-            for (int i = 0; i < surfaces.Length; i++)
-            {
-                surfaces[i].BuildNavMesh();
-            }
-        }
-        
-    }
-
-    public void Count()
-    {
-        count = 0;
-    }
+  //public NavMeshSurface[] surfaces;
+  //public Transform[] objectsToRotate;
+  //public int count;
+  //
+  //void Update()
+  //{        
+  //
+  //    for (int i = 0; i < surfaces.Length; i++)
+  //    {
+  //        //surfaces[i].BuildNavMesh();
+  //    }
+  //}
+  //
+  //public void BuildNavMesh()
+  //{
+  //    if(count<3)
+  //    count++;
+  //
+  //    if (count == 1)
+  //    {
+  //        for (int i = 0; i < surfaces.Length; i++)
+  //        {
+  //            surfaces[i].BuildNavMesh();
+  //        }
+  //    }
+  //    
+  //}
+  //
+  //public void Count()
+  //{
+  //    count = 0;
+  //}
 }
